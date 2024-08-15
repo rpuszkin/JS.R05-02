c371c61eab1fa3a978ae76409b2e1f104c100154
rpuszkin <130287042+rpuszkin@users.noreply.github.com>
2024-08-15 20:14:48 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function sentenceToUpperCase(sentence) {
+  return sentence.toUpperCase();
+}
+//przykładowy input:
+const result1 = sentenceToUpperCase("Ala ma kota");
+const result2 = sentenceToUpperCase("Raz dwa trzy cztery");
+console.log(result1, result2);
